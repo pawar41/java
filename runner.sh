b4170c7e81884aeff90f3c55eb936e3f57febbbe
pawar41 <shantanup410@gmail.com>
2022-08-09 00:16:07 +0530

    auto push user -shawn

--- runner.sh
+++ runner.sh
@@ -1,11 +1,11 @@
 for FILE in programmes/*.java 
 do 
     echo ""
-    echo "---------"
+    echo "======================"
     echo $(basename $(basename $FILE programmes/) .java)
-    echo "--------"
+    echo "----------------------"
     javac $FILE
     cd programmes
     java $(basename $FILE .java)
-    echo "----------------------"
+    echo "======================"
 done
